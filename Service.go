697c36b189bb493b47c903ef436ad9eb96751e7f
caselongo <caselongo@gmail.com>
2021-05-19 00:45:07 +0200

    x

--- Service.go
+++ Service.go
@@ -7,14 +7,19 @@ import (
 	"errors"
 	"fmt"
 	"io/ioutil"
+	"math"
+	"math/rand"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 
 	errortools "github.com/leapforce-libraries/go_errortools"
 	ig "github.com/leapforce-libraries/go_integration"
 	utilities "github.com/leapforce-libraries/go_utilities"
 )
+
+const defaultMaxRetries uint = 5
 
 type Accept string
 
@@ -176,7 +181,7 @@ func (service *Service) HTTPRequest(httpMethod string, requestConfig *RequestCon
 		fmt.Println("------------------------")
 	}
 
-	response, e := utilities.DoWithRetry(&service.client, request, requestConfig.MaxRetries)
+	response, e := service.doWithRetry(&service.client, request, requestConfig.MaxRetries)
 
 	if ig.Debug() {
 		fmt.Printf("DEBUG - Response\n%v\n", response)
@@ -294,4 +299,76 @@ func (service *Service) RequestCount() int64 {
 
 func (service *Service) ResetRequestCount() {
 	service.requestCount = 0
+}
+
+// doWithRetry executes http.Request and retries in case of 500 range status code
+// see: https://developers.google.com/analytics/devguides/config/mgmt/v3/errors#handling_500_or_503_responses
+func (service *Service) doWithRetry(client *http.Client, request *http.Request, maxRetries *uint) (*http.Response, *errortools.Error) {
+	if client == nil || request == nil {
+		if ig.Debug() {
+			if client == nil {
+				fmt.Println("DEBUG - client is nil")
+				fmt.Println("------------------------")
+			}
+			if request == nil {
+				fmt.Println("DEBUG - request is nil")
+				fmt.Println("------------------------")
+			}
+		}
+		return nil, nil
+	}
+
+	retry := uint(0)
+	_maxRetries := defaultMaxRetries
+	if maxRetries != nil {
+		_maxRetries = *maxRetries
+	}
+
+	for retry <= _maxRetries {
+		if retry > 0 {
+			fmt.Printf("Starting retry %v for %s %s\n", retry, request.Method, request.URL.String())
+			waitSeconds := math.Pow(2, float64(retry-1))
+			waitMilliseconds := int(rand.Float64() * 1000)
+			time.Sleep(time.Duration(waitSeconds)*time.Second + time.Duration(waitMilliseconds)*time.Millisecond)
+		}
+
+		response, err := client.Do(request)
+		if ig.Debug() {
+			if err != nil {
+				fmt.Printf("DEBUG - client.Do - error\n%s\n", err.Error())
+				fmt.Println("------------------------")
+			}
+			if response == nil {
+				fmt.Println("DEBUG - client.Do - response is nil")
+				fmt.Println("------------------------")
+			}
+		}
+
+		statusCode := 0
+		if response != nil {
+			statusCode = response.StatusCode
+		}
+
+		if (statusCode == 500 || statusCode == 503) && retry < _maxRetries { // retry in case of status 500/503 (server error)
+			retry++
+		} else {
+			if err == nil && (statusCode/100 == 4 || statusCode/100 == 5) {
+				err = fmt.Errorf("Server returned statuscode %v", statusCode)
+			}
+
+			if err != nil {
+				e := new(errortools.Error)
+				e.SetRequest(request)
+				e.SetResponse(response)
+				e.SetMessage(err.Error())
+
+				return response, e
+			}
+
+			return response, nil
+		}
+	}
+
+	// should never reach this
+	return nil, nil
 }
